ed206f552f05a1ceb5cbdd17c8371af660ed6f30
고수림 <sulim0314@naver.com>
2023-11-20 14:47:21 +0900

    이게 왜 안올라갔지 TheFooter.vue

---
+++ frontend/src/components/layout/TheFooter.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <hr>
+    <div class="footer">
+        Copyright ⓒENJOY TRIP <br>
+        고객센터: 02)655-1234 <br>
+        대표 전화: 02)123-4567 | 주소: 서울특별시 강남구 역삼동 123
+    </div>
+</template>
+
+<style scoped>
+.footer {
+    text-align: center;
+    height: 270px;
+    padding-top: 70px;
+}
+</style>
